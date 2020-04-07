46b50948d8d7d913976bae92b34b95e2fd2b085b
cawhitecode <casoccerdude@gmail.com>
2020-04-06 20:07:20 -0500

    Initial commit

---
+++ src/app/components/go-to-top/go-to-top.component.ts
@@ -0,0 +1,37 @@
+import { Component, HostListener, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-go-to-top',
+  templateUrl: './go-to-top.component.html',
+  styleUrls: ['./go-to-top.component.css']
+})
+export class GoToTopComponent {
+isShow: boolean;
+  topPosToStartShowing = 100;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+      
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
+}
